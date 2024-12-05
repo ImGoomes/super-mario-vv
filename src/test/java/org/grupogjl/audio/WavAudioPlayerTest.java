c265f737a64c78873a31517c1ad16873d4497d99
Gabriel Gomes <32543593+ImGoomes@users.noreply.github.com>
2024-12-05 13:42:32 +0000

    added more test for audio: valid input audio and mocking for exception handling

--- src/test/java/org/grupogjl/audio/WavAudioPlayerTest.java
+++ src/test/java/org/grupogjl/audio/WavAudioPlayerTest.java
@@ -1,33 +1,40 @@
 package org.grupogjl.audio;
 
-import org.grupogjl.Main;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class WavAudioPlayerTest {
 
     @Test
-    public void testPlaySound_NonExistentFile() {
-        // Verify that non-existent file handling doesn't throw unhandled exception
+    void testPlaySound_ValidFile() {
+        assertDoesNotThrow(() -> WavAudioPlayer.playSound("gameSound.wav"));
+    }
+
+    @Test
+    public void testPlaySound_FileNotFound() {
         assertDoesNotThrow(() -> WavAudioPlayer.playSound("non_existent.wav"));
     }
 
     @Test
     public void testPlaySound_NullFile() {
-        // Verify that null file handling doesn't throw unhandled exception
         assertDoesNotThrow(() -> WavAudioPlayer.playSound(null));
+    }
+
+    @Test
+    void testPlaySound_ExceptionHandling() throws Exception {
+        String validFileName = "testSound.wav";
+
+        Mockito.mockStatic(AudioSystem.class);
+        when(AudioSystem.getClip()).thenThrow(new RuntimeException("Mock exception"));
+
+        assertDoesNotThrow(() -> WavAudioPlayer.playSound(validFileName), "The playSound method should not crash even if an exception occurs.");
     }
 }
