3ba61d9d09f37950114636eeebec6db950069e58
Gabriel Gomes <32543593+ImGoomes@users.noreply.github.com>
2024-12-06 09:25:04 +0000

    added test for ExitGameCommand

---
+++ src/test/java/org/grupogjl/commands/ExitGameCommandTests.java
@@ -0,0 +1,27 @@
+package org.grupogjl.commands;
+
+import org.grupogjl.Game;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExitGameCommandTests {
+
+    private ExitGameCommand exitGameCommand;
+    private Game mockGame;
+
+    @BeforeEach
+    void setUp() {
+        exitGameCommand = new ExitGameCommand();
+        mockGame = mock(Game.class);
+    }
+
+    @Test
+    void testExecuteSetsGameStateToNull() {
+        exitGameCommand.execute(mockGame);
+        verify(mockGame, times(1)).setStateNull();
+    }
+}
