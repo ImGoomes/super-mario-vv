7734fc25c418485c94dc27ff40e2db62dcefc361
Gabriel Gomes <32543593+ImGoomes@users.noreply.github.com>
2024-12-07 18:32:21 +0000

    added test for ResumeCommand class

---
+++ src/test/java/org/grupogjl/commands/ResumeCommandTests.java
@@ -0,0 +1,30 @@
+package org.grupogjl.commands;
+
+import org.grupogjl.Game;
+import org.grupogjl.state.StatePause;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+class ResumeCommandTests {
+
+    private ResumeCommand resumeCommand;
+    private Game mockGame;
+    private StatePause mockStatePause;
+
+    @BeforeEach
+    void setup() {
+        resumeCommand = new ResumeCommand();
+        mockGame = mock(Game.class);
+        mockStatePause = mock(StatePause.class);
+
+        when(mockGame.getStatePause()).thenReturn(mockStatePause);
+    }
+
+    @Test
+    void testExecuteSetsGameStateToResume() {
+        resumeCommand.execute(mockGame);
+        verify(mockGame, times(1)).setStateGame(mockStatePause.getParent());
+    }
+}
