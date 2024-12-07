53e95905ade84de8533535891cc52192fc107caf
Gabriel Gomes <32543593+ImGoomes@users.noreply.github.com>
2024-12-07 19:07:28 +0000

    added class to startGameCommand class

---
+++ src/test/java/org/grupogjl/commands/StartGameCommandTests.java
@@ -0,0 +1,49 @@
+package org.grupogjl.commands;
+
+import org.grupogjl.Game;
+import org.grupogjl.state.StateGame;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+class StartGameCommandTests {
+
+    private StartGameCommand startGameCommand;
+    private Game mockGame;
+    private StateGame mockStateGame;
+
+    @BeforeEach
+    void setup() {
+        startGameCommand = new StartGameCommand();
+        mockGame = mock(Game.class);
+        mockStateGame = mock(StateGame.class);
+
+        when(mockGame.getStateGame()).thenReturn(mockStateGame);
+    }
+
+    @Test
+    void testExecuteSetsGameStateToStart() throws IOException{
+        startGameCommand.execute(mockGame);
+        verify(mockGame, times(1)).setStateGame();
+        verify(mockStateGame, times(1)).setGameOver(false);
+    }
+
+    @Test
+    void testExecuteThrowsIOException() throws IOException {
+        doThrow(new IOException("Mock IOException")).when(mockGame).setStateGame();
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> startGameCommand.execute(mockGame),
+                "The execute method should throw a RuntimeException when setStateGame throws an IOException.");
+        assertEquals("java.io.IOException: Mock IOException", exception.getMessage());
+    }
+
+    @Test
+    void testExecuteSetsNullGame() {
+        assertThrows(NullPointerException.class, () -> startGameCommand.execute(null), "Executing with a null game should throw a NullPointerException.");
+    }
+}
