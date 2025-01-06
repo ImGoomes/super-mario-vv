1af1490ca32043df0114eb7d5b4c083d2be721eb
Gabriel Gomes <gabrielsousagomes@hotmail.com>
2025-01-06 11:30:31 +0000

    refactoring audio class

--- src/test/java/org/grupogjl/audio/WavAudioPlayerTest.java
+++ src/test/java/org/grupogjl/audio/WavAudioPlayerTest.java
@@ -1,27 +1,51 @@
 package org.grupogjl.audio;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(MockitoExtension.class)
-class WavAudioPlayerTest {
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
-    @Test
-    void testPlaySound_ValidFile() {
-        assertDoesNotThrow(() -> WavAudioPlayer.playSound("gameSound.wav"));
+public class WavAudioPlayerTest {
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setErr(new PrintStream(errContent));
     }
 
     @Test
-    public void testPlaySound_FileNotFound() {
-        assertDoesNotThrow(() -> WavAudioPlayer.playSound("non_existent.wav"));
+    public void testPlayInvalidSound() {
+        WavAudioPlayer.playSound("non-existent.wav");
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            fail("Test interrupted");
+        }
+
+        assertFalse(errContent.toString().isEmpty());
     }
 
     @Test
-    public void testPlaySound_NullFile() {
-        assertDoesNotThrow(() -> WavAudioPlayer.playSound(null));
+    public void testPlayValidSound() {
+        WavAudioPlayer.playSound("test-sound.wav");
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            fail("Test interrupted");
+        }
+
+        assertFalse(errContent.toString().isEmpty());
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setErr(originalErr);
+        errContent.reset();
     }
 }
